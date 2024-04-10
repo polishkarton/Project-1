d1e43656bf4126eeb8aeb28df26951a8e777aa38
Zosia Samosia <Zosia@Samosia.pl>
2024-04-10 17:01:33 +0200

    newest navbar

--- js/scripts.js
+++ js/scripts.js
@@ -4,10 +4,10 @@ const menu = document.querySelector('span');
 const dropdown = document.querySelector('#nav-box');
 
 menu.onclick = function() {
-    if(menu.innerHTML == 'menu') {
+    if(menu.innerHTML == 'menu'){
         menu.innerHTML = 'close';
         dropdown.classList.add('active')
-    }else{
+    }else {
         menu.innerHTML = 'menu';
         dropdown.classList.remove('active')
     }
