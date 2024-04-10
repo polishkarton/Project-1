2199b49b048510cbee7dbcb2c72d9c197d755e7f
Zosia Samosia <Zosia@Samosia.pl>
2024-04-10 16:34:15 +0200

    add navbar plus menu

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,15 @@
 'use strict';
+
+const menu = document.querySelector('span');
+const dropdown = document.querySelector('#nav-box');
+
+menu.onclick = function() {
+    if(menu.innerHTML == 'menu') {
+        menu.innerHTML = 'close';
+        dropdown.classList.add('active')
+    }else{
+        menu.innerHTML = 'menu';
+        dropdown.classList.remove('active')
+    }
+};
+
